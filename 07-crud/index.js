e90c7dc2032d746ba50a46efd7e5c63351f3ef16
kunxin-chor <chorkunxin@yahoo.com>
2022-02-18 03:45:40 +0000

    Update food record

--- 07-crud/index.js
+++ 07-crud/index.js
@@ -31,6 +31,63 @@ app.get('/', async function(req,res){
     })
 })
 
+app.get('/sightings/create', async function(req,res){
+    res.render('create-food')
+})
+
+app.post("/sightings/create", async function(req,res){
+    // res.send(req.body); // echo back the form data
+    // let description = req.body.description;
+    // let food = req.body.food;
+    // let datetime = req.body.datetime;
+    let {description, food, datetime } = req.body;  // object destructuring
+
+    // let payload = {
+    //     'description': description, // where the key of the property matches the variable name of the value
+    //     'food' : food,
+    //     'datetime': datetime
+    // }
+    
+    food = food.split(',');  // return an array of string and reassign back to food
+    let payload = {description, food, datetime }; // it means the key of each property 
+                                                  // is the name of the variable
+
+    // axios.post send a POST request to the endpoint
+    // first arg: endpoint URL
+    // second arg: the body (or the data to send)
+    await axios.post( BASE_API_URL + "/sighting", payload);
+
+    res.redirect('/'); // send a response back to the client
+                       // and tell it to automatically go to / route
+
+})
+
+// example: update the sightings with id 123456
+// then the url will look like: /sightings/123456/update
+app.get('/sightings/:sighting_id/update', async function(req,res){
+    let sightingId = req.params.sighting_id;
+    let response = await axios.get(BASE_API_URL + '/sighting/' + sightingId);
+    
+    let sighting = response.data;
+    sighting.datetime = sighting.datetime.slice(0, -1); // remove the last character
+                                                        // needed to display the sighting date time correctly
+    
+    res.render('update-food', {
+        'sighting': sighting
+    })                                                    
+})
+
+app.post('/sightings/:sighting_id/update', async function(req,res){
+    let payload = {
+        'description': req.body.description,
+        'food': req.body.food.split(','),
+        'datetime': req.body.datetime
+    }
+    let sightingId = req.params.sighting_id;
+    await axios.put(BASE_API_URL + '/sighting/' + sightingId, payload);
+    res.redirect('/');
+})
+
 // BEGIN SERVER (aka LISTEN)
 app.listen(3000, function(){
     console.log("server begins");
